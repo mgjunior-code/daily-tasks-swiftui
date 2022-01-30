aab742312cc755b57c48ece95b87147831652aab
Marcelo Goncalves De Sousa Junior <marcelog.junior@fornecedores.sicoob.com.br>
2022-01-30 12:33:32 -0300

    Initial Commit

---
+++ DailyTasks/DailyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTasksApp.swift
+//  DailyTasks
+//
+//  Created by Marcelo Junior on 30/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
